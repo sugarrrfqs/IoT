def822d5d6f2625d975100dfdc4031019ed00037
sugarrrfqs <78405130+sugarrrfqs@users.noreply.github.com>
2023-05-09 16:15:55 +0500

    Добавлен прием команд от мобильного клиента и передача команд сканеру, добавлены семафоры для синхронизации тредов

--- Server/serv.c
+++ Server/serv.c
@@ -8,11 +8,19 @@
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 void* func (void* arg);
 
+int command = -1;
+char* filename = "/home/sgrrr/IoT/serverWifiInfo.txt";
+char eom[256] = "---";
+sem_t semaphore;
+
+
 int main()
 {	
+	sem_init(&semaphore, 0, 1);
 	struct sockaddr_in adr;
 	int socketL = socket(AF_INET, SOCK_STREAM, 0);		
 	adr.sin_family = AF_INET;
@@ -45,65 +53,84 @@ int main()
 		
 	pthread_t id;
 	int socketC;
+	sem_wait(&semaphore);
 	while (socketC!= -1)
 	{
 		socketC = accept(socketL, NULL, NULL);
 		pthread_create(&id, NULL, func, &socketC);
 	}
 	close (socketL);
+	sem_destroy(&semaphore);
 			
 	return 0;				
 }
+
 
 void* func (void* arg)
 {
 	int socket = *(int*)arg;
 	int clientType = 0;
 	char str[256] = "s";
-	char eom[256] = "---";
-	char* filename = "/home/sgrrr/IoT/serverWifiInfo.txt";
 		
 	read (socket, &clientType, sizeof(clientType));
 	
 	if (clientType == 1) // сканер wifi
 	{
-		system("echo wifi scan connected");
-				
-		while(read (socket, &str, 256) > 0)
+		system("echo wifi scan connected");	
+					
+		while(socket > 0)
 		{	
-			FILE *fp = fopen(filename, "w");
-			if(fp)
+			sem_wait(&semaphore);	
+			if (command == 1)
 			{
-				while (strcmp(str, eom) != 0)
-				{				
-					fputs(str, fp);	
-					read (socket, &str, 256);					
-				}
-				fclose(fp);
-				system("echo ----- wifi info received");
-				sleep(5);			
-			}		
+				system("echo ----- request received from mobile client");
+				write(socket, &command, sizeof(command));
+				system("echo ----- request sended to wifi scaner");
+				
+				FILE *fp = fopen(filename, "w");
+				if(fp)
+				{
+					while (read (socket, &str, 256) > 0 && strcmp(str, eom) != 0)
+					{	
+						
+						//printf("%s\n", str);	
+						fputs(str, fp);											
+					}
+					fclose(fp);
+					system("echo ----- wifi info received from wifi scaner");			
+				}			
+			}
+			sem_post(&semaphore);	
+			sleep(3);
 		}
+		
 		system("echo wifi scan disconnected");
 	}
+	
 	if (clientType == 2) // мобильный клиент
 	{
 		system("echo mobile client connected");
 		
-		while(read (socket, &str, 256) > 0)
-		{				
-			FILE *fp = fopen(filename, "r");
-			if(fp)
-			{
-				while(fgets(str, 256, fp) != NULL)
-				{				
-					write(socket, str, sizeof(str));
-				}
-				write(socket, eom, sizeof(eom));
-				fclose(fp);
-			} 								
-			system("echo ----- wifi info sended");
-			sleep(5);			
+		while(read (socket, &command, sizeof(command)) > 0)
+		{	
+			sem_post(&semaphore);
+			sleep(1);
+			sem_wait(&semaphore);			
+			if (command == 1)
+			{	
+				FILE *fp = fopen(filename, "r");
+				if(fp)
+				{
+					while(fgets(str, 256, fp) != NULL)
+					{				
+						write(socket, str, sizeof(str));
+					}
+					write(socket, eom, sizeof(eom));
+					fclose(fp);
+				} 								
+				system("echo ----- wifi info sended to mobile client\n\n");
+				sleep(5);
+			}			
 					
 		}
 		system("echo mobile client disconnected");
